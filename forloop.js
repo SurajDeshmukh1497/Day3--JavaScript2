1496e41f40f7f206e7d6f54760cd5c3c60384a8c
SurajDeshmukh1497 <suraj.deshmukh.1997@gmail.com>
2024-07-04 10:27:06 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,8 @@
+// Use a for loop to repeat a block of code 10 times
+for (var count = 0; count < 10; count++) {
+    // Print the current value of 'count' to the console
+    console.log("Current Count: " + count);
+}
+
+// Print a message to the console after the loop finishes
+console.log("Loop stopped!");
